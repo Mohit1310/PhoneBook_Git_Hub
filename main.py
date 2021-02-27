e11d5bd80d9bd07a90f68c0d4ce22f6095704b92
Mohit1310 <mohitdayama1234@gmail.com>
2021-02-27 16:33:48 +0530

    First commit

--- main.py
+++ main.py
@@ -18,16 +18,37 @@ def phonebook():
         option = welcome()
         if option == 1:
             contact_name = input("Enter contact name in this format 'First name  Last name': ")
-            contact_number = int(input("Enter your number"))
+            contact_number = int(input("Enter your number: "))
+
+            if contact_number in contact.values():
+                print("Contact already exists!!!")
+            else:
+                contact[contact_name]=contact_number
 
         if option == 2:
-            if bool(contact) !=False:
-                
-        if option == 3:
-            pass
-        if option == 4:
-            pass
-        if option == 5:
-            pass
+            if bool(contact) != False
+                for k,v in contact.items():
+                    print("{} ==> {}".format(k,v))
+            else:
+                print("No Contacts exist !!! Your PhoneBook is empty.")
 
-if __main__ =='__name__'
+        if option == 3:
+            name = input("Enter the name of contact details: ")
+            if name in contact:
+                print("{} ==> {}".format(name,contact[name]))
+            else:
+                print("No contacts exist with {}  name.".format(name))
+
+        if option == 4:
+            name = input("Enter the name of Contact Details that you wish to delete: ")
+            if name in contact:
+                contact.pop(name)
+            else:
+                print("No contact exist with name {} ".format(name))
+
+        if option == 5:
+            print("Thanks for using out Phonebook App")
+            break
+
+if __name__ =='__main__':
+    phonebook()
